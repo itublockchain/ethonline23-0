f35a522cf54fcc8e42c0d028be806b0c0d824be7
Ali Eray <alieraay@gmail.com>
2023-10-20 03:02:03 +0300

    update

--- blockchain/contracts/mocks/MockBAYC.sol
+++ blockchain/contracts/mocks/MockBAYC.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MockBAYC is ERC721 {
