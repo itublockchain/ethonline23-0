20db110d6d26df628d8fb6eb0d7d189c4fe7dc5f
koybasimuhittin <koybasimuhittin@gmail.com>
2023-10-21 20:40:25 +0300

    fix persistent connection

--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -6,21 +6,29 @@ const UserProvider: React.FC<{ children: React.ReactNode }> = ({
 	children,
 }) => {
 	const [user, setUser] = React.useState<User | null>(null)
-	const [connected, setConnected] = React.useState<boolean>(
-		window.localStorage.getItem("connected")
-			? JSON.parse(localStorage.getItem("connected") as string)
-			: false
-	)
+	const [connected, setConnected] = React.useState<boolean>(false)
 
 	React.useEffect(() => {
 		if (user && !connected) {
 			setConnected(true)
 			window.localStorage.setItem("connected", JSON.stringify(true))
-		} else if (!user && connected) {
+		}
+	}, [connected, user])
+
+	React.useEffect(() => {
+		if (!user && connected) {
 			setConnected(false)
 			window.localStorage.setItem("connected", JSON.stringify(false))
 		}
-	}, [connected, user])
+	}, [user])
+
+	React.useEffect(() => {
+		setConnected(
+			window.localStorage.getItem("connected")
+				? JSON.parse(localStorage.getItem("connected") as string)
+				: false
+		)
+	}, [])
 
 	return (
 		<UserContext.Provider
