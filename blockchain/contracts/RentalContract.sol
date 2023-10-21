ee2f31826b991c201a0a20b9e3e93bd268861f4f
Ali Eray <alieraay@gmail.com>
2023-10-21 04:22:48 +0300

    endRental

--- blockchain/contracts/RentalContract.sol
+++ blockchain/contracts/RentalContract.sol
@@ -8,16 +8,16 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract RentalContract {
-    IERC721 public bayc;  // ERC721 token contract address (e.g., Bored Ape Yacht Club)
-    IERC20 public apeCoin;  // ERC20 token contract address used for payments
-    address public vaultAddress;  // Vault address where half of the rental payment is sent
+    IERC721 public bayc; // ERC721 token contract address (e.g., Bored Ape Yacht Club)
+    IERC20 public apeCoin; // ERC20 token contract address used for payments
+    address public vaultAddress; // Vault address where half of the rental payment is sent
 
     // Struct to hold rental information.
     struct Rental {
-        address owner;  // Owner of the ERC721 token
-        address renter;  // Renter of the ERC721 token
-        uint256 price;  // Price for the rental
-        uint256 endTime;  // Expiry timestamp of the rental
+        address owner; // Owner of the ERC721 token
+        address renter; // Renter of the ERC721 token
+        uint256 price; // Price for the rental
+        uint256 endTime; // Expiry timestamp of the rental
     }
 
     // Mapping to hold rental information against each tokenId.
@@ -26,11 +26,17 @@ contract RentalContract {
     mapping(address => uint256) public addressToTokenId;
 
     // Event emitted when a new rental is listed.
-    event RentalListed(uint256 indexed tokenId, address indexed owner, uint256 price, uint256 endTime);
+    event RentalListed(
+        uint256 indexed tokenId,
+        address indexed owner,
+        uint256 price,
+        uint256 endTime
+    );
     // Event emitted when a rental starts.
     event RentalStarted(uint256 indexed tokenId, address indexed renter);
-    // Event emitted when a rental ends.
-    event RentalEnded(uint256 indexed tokenId, address indexed renter);
+
+    // Event emitted when all rentals have ended.
+    event AllRentalsEnded();
 
     // Constructor initializes the contract with the addresses of ERC721, ERC20 contracts, and the vault.
     constructor(address _bayc, address _apeCoin, address _vaultAddress) {
@@ -45,21 +51,26 @@ contract RentalContract {
         uint256 price,
         uint256 duration
     ) external {
-        require(bayc.ownerOf(tokenId) == msg.sender, "Not owner");  // Ensure caller is the owner of the token
+        require(bayc.ownerOf(tokenId) == msg.sender, "Not owner"); // Ensure caller is the owner of the token
         rentals[tokenId] = Rental({
             owner: msg.sender,
             renter: address(0),
             price: price,
             endTime: block.timestamp + duration
         });
-        emit RentalListed(tokenId, msg.sender, price, block.timestamp + duration);  // Emit event
+        emit RentalListed(
+            tokenId,
+            msg.sender,
+            price,
+            block.timestamp + duration
+        ); // Emit event
     }
 
     // Function to rent an ERC721 token listed for rent.
     function rent(uint256 tokenId) external {
-        Rental storage rental = rentals[tokenId];  // Get rental info
-        require(rental.owner != address(0), "Not listed");  // Ensure token is listed for rent
-        require(rental.renter == address(0), "Already rented");  // Ensure token is not already rented
+        Rental storage rental = rentals[tokenId]; // Get rental info
+        require(rental.owner != address(0), "Not listed"); // Ensure token is listed for rent
+        require(rental.renter == address(0), "Already rented"); // Ensure token is not already rented
 
         // Transfer half of the rental price to the owner and the other half to the vault.
         require(
@@ -70,19 +81,24 @@ contract RentalContract {
             apeCoin.transferFrom(msg.sender, vaultAddress, rental.price / 2),
             "Payment to vault failed"
         );
-        rental.renter = msg.sender;  // Update renter
-        emit RentalStarted(tokenId, msg.sender);  // Emit event
+        rental.renter = msg.sender; // Update renter
+        emit RentalStarted(tokenId, msg.sender); // Emit event
     }
 
-    // Function to end the rental after the rental period has ended.
-    function endRental(uint256 tokenId) external {
-        Rental storage rental = rentals[tokenId];  // Get rental info
-        require(
-            block.timestamp >= rental.endTime,
-            "Rental period not ended yet"
-        );  // Ensure rental period has ended
-        rental.renter = address(0);  // Reset renter
-        emit RentalEnded(tokenId, msg.sender);  // Emit event
+    // Function to end all rentals and make all NFTs available for rent again.
+    // Function to end all rentals and make all NFTs available for rent again.
+    function endAllRentals() external {
+        // Iterate through all possible token IDs (assuming a maximum tokenId for simplicity)
+        // Note: You would need to adapt this to match the actual total supply of your NFTs.
+        for (uint256 tokenId = 1; tokenId <= 20; tokenId++) {
+            // Check if a rental exists for the current tokenId
+            if (rentals[tokenId].owner != address(0)) {
+                // Reset the renter field to address(0) to make the NFT available for rent again
+                rentals[tokenId].renter = address(0);
+            }
+        }
+        // Emit an event to notify that all rentals have ended
+        emit AllRentalsEnded();
     }
 
     // Function to check if a token is rented.
